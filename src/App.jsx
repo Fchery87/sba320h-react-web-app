a76d8cf8184e84322e3f5de3ae22b77204b8df4e
Frantz Chery <frantzchery@hotmail.com>
2024-06-13 22:10:08 -0400

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,19 @@ const App = () => {
 
   useEffect(() => {
     const fetchNews = async () => {
-      const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=7ead49c10e4d4bcca3ead8a2695128d9&pageSize=20');
-      const data = await response.json();
-      const newsWithImages = data.articles.filter(article => article.urlToImage);
-      setNews(newsWithImages);
-      setFilteredNews(newsWithImages);
+      try {
+        const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=7ead49c10e4d4bcca3ead8a2695128d9&pageSize=20');
+        const data = await response.json();
+        if (data && data.articles) {
+          const newsWithImages = data.articles.filter(article => article.urlToImage);
+          setNews(newsWithImages);
+          setFilteredNews(newsWithImages);
+        } else {
+          console.error('Invalid data structure:', data);
+        }
+      } catch (error) {
+        console.error('Error fetching news:', error);
+      }
     };
 
     fetchNews();
